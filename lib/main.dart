e4e9b76d2777322b7de7921ce9ae0d5461d43b34
Aspirers <nali@leeddev.io>
2022-08-27 07:33:00 -0700

    Stateful Widget basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() =>
+    runApp(MaterialApp(
       home: MyCard(),
     ));
 
-class MyCard extends StatelessWidget {
+/*
+* Action menu's are really helping materials in flutter
+* */
+class MyCard extends StatefulWidget {
   const MyCard({Key? key}) : super(key: key);
+
+  @override
+  State<MyCard> createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+
+  int developmentLevel = 0;
+
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +29,15 @@ class MyCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() { // it'll change the state now
+            developmentLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[850],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -29,7 +51,7 @@ class MyCard extends StatelessWidget {
               ),
             ),
             Divider(height: 90.0,
-            color:Colors.grey[800],
+              color: Colors.grey[800],
             ),
             Text(
               "NAME",
@@ -51,7 +73,7 @@ class MyCard extends StatelessWidget {
               style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
             ),
             Text(
-              "Beginner",
+              '$developmentLevel',
               style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2.0,
@@ -68,15 +90,15 @@ class MyCard extends StatelessWidget {
             Row(
               children: [
                 Icon(Icons.email,
-                color: Colors.grey[400],
+                  color: Colors.grey[400],
                 ),
                 SizedBox(width: 10.0,),
                 Text(
                   'i_numn@yahoo.com',
                   style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0
                   ),
                 ),
               ],
@@ -87,3 +109,5 @@ class MyCard extends StatelessWidget {
     );
   }
 }
+
+
